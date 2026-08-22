3eeb27cbccac1b3b909809d22dd2901c2673d1c5
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-23 03:42:08 +0600

    feat(feed): Add default children, Feed component

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,13 @@
 import App from "./App";
 import ErrorPage from "./ErrorPage";
 
+import Feed from "./components/feed/Feed";
+
 export const routes = [
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [{ index: true, element: <Feed /> }],
   },
 ];
