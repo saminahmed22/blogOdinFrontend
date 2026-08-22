25e42760814ce8e5a65cde41545c8132b53a9c98
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-23 03:42:44 +0600

    feat: Add post and profile routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,12 +2,18 @@ import App from "./App";
 import ErrorPage from "./ErrorPage";
 
 import Feed from "./components/feed/Feed";
+import Post from "./components/post/post";
+import Profile from "./components/profile/profile";
 
 export const routes = [
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <Feed /> }],
+    children: [
+      { index: true, element: <Feed /> },
+      { path: "posts/:postId", element: <Post /> },
+      { path: "profile", element: <Profile /> },
+    ],
   },
 ];
