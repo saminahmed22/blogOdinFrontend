608b5f86f6c924d630ac34729276dc06ad562499
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-24 01:00:22 +0600

    feat: Popover component for profile actions

---
+++ src/components/profileActionPopover/ProfileActionPopover.jsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+
+import tempProfilePicture from "../../assets/images/image.png";
+
+import styles from "./ProfileActionPopover.module.css";
+
+export default function ProfileActionPopover() {
+  return (
+    <>
+      <button
+        title="Profile actions"
+        className={`${styles.profileActBtn}`}
+        popoverTarget="ProfileActionPopover"
+        popoverTargetAction="toggle"
+        onClick={() => console.log("clicked")}
+      >
+        <img src={tempProfilePicture} alt="Profile picture" />
+      </button>
+
+      <dialog
+        id="ProfileActionPopover"
+        className={styles.ProfileActionPopover}
+        popover="auto"
+      >
+        <Link to="/profile/1">Profile</Link>
+        <Link to="/auth/logout">Logout</Link>
+      </dialog>
+    </>
+  );
+}
