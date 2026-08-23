dd2617b122c3f469f0e3e67c067de2eac3c4b8ad
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-24 01:01:46 +0600

    feat(nav): Add navbar for action buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,50 @@
-export default function Header() {
+import { Link } from "react-router";
+
+import styles from "./Header.module.css";
+
+// Icons
+import addIcon from "../../assets/icons/add.svg";
+import loginIcon from "../../assets/icons/login.svg";
+
+// Components
+import ProfileActionPopover from "../profileActionPopover/ProfileActionPopover";
+
+export default function Header({ authStatus = true }) {
+  const getNavElements = () => {
+    if (authStatus) {
+      return (
+        <>
+          <button
+            className={`${styles.navPrimaryBtn} createPostBtn`}
+            title="Create a post"
+          >
+            <img src={addIcon} alt="Add icon" />
+            Create a post
+          </button>
+
+          <ProfileActionPopover />
+        </>
+      );
+    } else {
+      return (
+        <button
+          className={`${styles.navPrimaryBtn} loginBtn`}
+          title="Login or Sign up"
+        >
+          <img src={loginIcon} alt="Login icon" />
+          Login/Sign up
+        </button>
+      );
+    }
+  };
+
   return (
-    <header>
-      <h1>BlogOdin</h1>
+    <header className={styles.header}>
+      <Link className={styles.homeRedirectLink} to={"/"}>
+        <h1 title="Return to the homepage">BlogOdin</h1>
+      </Link>
+
+      <nav className={styles.navbar}>{getNavElements()}</nav>
     </header>
   );
 }
