f3f39c8f4276f15536e82ba597a4e4064452a5b6
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-24 03:48:41 +0600

    feat: Add PostCardAction components

---
+++ src/components/postCard/postCardActBtn/PostCardActBtn.jsx
@@ -0,0 +1,29 @@
+import styles from "./PostCardActBtn.module.css";
+
+import optionsIcon from "../../../assets/icons/options.svg";
+
+export default function PostCardActionPopver() {
+  return (
+    <>
+      <button
+        title="Post card actions"
+        className={`${styles.postCardActBtn}`}
+        popoverTarget="postCardActPopover"
+        popoverTargetAction="toggle"
+      >
+        <img src={optionsIcon} alt="Options icon" />
+      </button>
+
+      <dialog
+        id="postCardActPopover"
+        className={styles.postCardActPopover}
+        popover="auto"
+      >
+        <button className={styles.editBtn}>Edit</button>
+        <button className={styles.deleteBtn}>Delete</button>
+        <button className={styles.saveBtn}>Save</button>
+        <button className={styles.reportBtn}>Report</button>
+      </dialog>
+    </>
+  );
+}
