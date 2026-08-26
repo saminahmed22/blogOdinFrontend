68426bbbd3930abf8f6f4e948383ca14d071fa81
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-27 04:06:19 +0600

    feat: Render skeleton or error message based on fetch condition

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
+
 import styles from "./Feed.module.css";
 
-export default function Feed(){
-    return (
-        <div className={styles.feed}>Feed div</div>
-    )
+// Componenets
+import PostCard from "../postCard/PostCard";
+
+// API
+import { fetchFeedContent } from "../../api/fetchFeedContent";
+
+export default function Feed() {
+  let [feedContent, setFeedContent] = useState({
+    status: "fetching",
+    posts: [],
+  });
+
+  useEffect(() => {
+    fetchFeedContent(10).then((response) => {
+      setFeedContent({ status: response.status, posts: response.posts });
+    });
+  }, []);
+
+  const getFeedContent = () => {
+    if (feedContent.status === "fetching") {
+      return (
+        <>
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+        </>
+      );
+    } else if (feedContent.status === 200) {
+      return feedContent.posts.map((post) => (
+        <PostCard key={post.id} loading={false} />
+      ));
+    } else {
+      return (
+        <div className={styles.feedErrorMessage} data-testid="feedErrorMessage">
+          An error occured. Please try again later.
+        </div>
+      );
+    }
+  };
+
+  return <div className={styles.feed}>{getFeedContent()}</div>;
 }
