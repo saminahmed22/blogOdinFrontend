791384b7e89ccde43f30487ddb8dab6c5614c150
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-27 04:03:02 +0600

    feat(theme): Add a theme switch button

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,49 @@ import { Outlet } from "react-router";
 
 // Components
 import Header from "./components/header/Header";
+import { useState } from "react";
 
 export default function App() {
+  let [theme, setTheme] = useState("light");
+
   return (
     <>
       <Header />
       <Outlet />
+      <button
+        className="themeBtn"
+        onClick={() => {
+          if (theme === "light") {
+            document.body.style.background = "rgb(40, 40, 40)";
+
+            const postCards = document.querySelectorAll(".postCard");
+
+            postCards.forEach((postCard) => {
+              postCard.style.color = "white";
+              postCard.style.background = "rgb(29 29 29)";
+            });
+
+            setTheme("dark");
+
+            localStorage.setItem("theme", "dark");
+          } else {
+            document.body.style.background = "white";
+
+            const postCards = document.querySelectorAll(".postCard");
+
+            postCards.forEach((postCard) => {
+              postCard.style.color = "black";
+              postCard.style.background = "white";
+            });
+
+            setTheme("light");
+
+            localStorage.setItem("theme", "light");
+          }
+        }}
+      >
+        Click
+      </button>
     </>
   );
 }
