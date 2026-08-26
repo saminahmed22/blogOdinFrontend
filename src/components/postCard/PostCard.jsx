4e8870e51edfa9af5ca44b4edcb5374507256b02
Samin Ahmed <saminahmed1001@gmail.com>
2026-08-27 04:14:18 +0600

    feat: Add loading skeleton

--- src/components/postCard/PostCard.jsx
+++ src/components/postCard/PostCard.jsx
@@ -1,23 +1,54 @@
 import styles from "./PostCard.module.css";
 
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 import tempProfilePicture from "../../assets/images/image.png";
 // Components
 import PostCardActionPopver from "./postCardActBtn/PostCardActBtn";
 
-export default function PostCard() {
-  return (
-    <article className={styles.postCard}>
-      <PostCardActionPopver />
+export default function PostCard({ loading = true }) {
+  const navigate = useNavigate();
 
-      <h3 className={styles.postCardTitle}>
-        <Link to={"/posts/123"}>This is a title</Link>
-      </h3>
-
-      <div className={styles.postDetails}>
-        <Link to={"/profile/Samin Ahmed"}>
+  if (loading) {
+    return (
+      <div
+        className={`postCard ${styles.postCard}`}
+        data-testid="postCardSkeleton"
+      >
+        <div className={styles.postDetails}>
           <div className={styles.authorDetails}>
+            <div
+              src={tempProfilePicture}
+              className={styles.authorImageLoading}
+            ></div>
+            <address className={styles.authorNameLoading}></address>
+          </div>
+        </div>
+
+        <div className={styles.postCardArticle}>
+          <h3 className={styles.postCardTitleLoading}></h3>
+
+          <div className={styles.skeletonPreviewContainer}>
+            <p className={styles.postCardPreviewLoading}></p>
+            <p className={styles.postCardPreviewLoading}></p>
+            <p className={styles.postCardPreviewLoading}></p>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={`${styles.postCard} postCard`} data-testid="postCard">
+        <div className={styles.postCardHeader}>
+          <div
+            className={styles.authorDetails}
+            onClick={(e) => {
+              e.stopPropagation();
+              e.preventDefault();
+
+              navigate("/profile/Samin Ahmed");
+            }}
+          >
             <img
               src={tempProfilePicture}
               alt="Author's profile picture"
@@ -31,23 +62,25 @@ export default function PostCard() {
               Samin Ahmed
             </address>
           </div>
-        </Link>
 
-        <div className={styles.postDates}>
-          <p className={styles.postCardDate} data-testid="post-upload-date">
-            Created at: August 10, 2026
-          </p>
-          &bull;
-          <p className={styles.postCardDate} data-testid="post-expiry-date">
-            Last modified: August 15, 2026
-          </p>
+          <PostCardActionPopver />
         </div>
-      </div>
 
-      <p className={styles.postCardPreview} data-testid="post-preview">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil laborum
-        fugiat hic magna.
-      </p>
-    </article>
-  );
+        <Link to={"/posts/123"} style={{ textDecoration: "none" }}>
+          <article className={styles.postCardArticle}>
+            <h3 className={styles.postCardTitle}>
+              10 Habits That Quietly Make You a Better Developer
+            </h3>
+
+            <p className={styles.postCardPreview} data-testid="post-preview">
+              From reading other people's code to embracing the discomfort of
+              not knowing — small, consistent habits that compound into real
+              growth as a developer. A quick read for anyone stuck in a learning
+              plateau
+            </p>
+          </article>
+        </Link>
+      </div>
+    );
+  }
 }
