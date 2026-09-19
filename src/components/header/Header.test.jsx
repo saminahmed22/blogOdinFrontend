0b0eeb44d9d057e344469ebe89e1cdb286cd6a41
Samin Ahmed <saminahmed1001@gmail.com>
2026-09-19 16:10:39 +0600

    refactor: Remove irrelevent tests

--- src/components/header/Header.test.jsx
+++ src/components/header/Header.test.jsx
@@ -2,8 +2,6 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 
-import { getItemSpy } from "../../../testsSetup";
-
 import Header from "./Header";
 
 describe("Header component", () => {
@@ -14,76 +12,6 @@ describe("Header component", () => {
       </MemoryRouter>,
     );
 
-    expect(screen.getByRole("heading").textContent).toMatch(/BlogOdin/i);
-  });
-
-  it("Calls localStorage to fetch JWT data", () => {
-    getItemSpy.mockImplementationOnce((key) => {
-      return key === "jwt" ? null : true;
-    });
-
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>,
-    );
-
-    expect(getItemSpy).toHaveBeenCalledWith("jwt");
-  });
-
-  it("Renders Login/sign up button when user is not logged in", () => {
-    getItemSpy.mockImplementationOnce((key) => {
-      return key === "jwt" ? null : true;
-    });
-
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>,
-    );
-
-    expect(screen.getByTestId("loginBtn")).toBeInTheDocument();
-  });
-
-  it("Does not renders Login/sign up button when user is logged in", () => {
-    getItemSpy.mockImplementationOnce((key) => {
-      return key === "jwt" ? true : null;
-    });
-
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>,
-    );
-
-    expect(screen.queryByTestId("loginBtn")).not.toBeInTheDocument();
-  });
-
-  it("Renders post button when user is logged in", () => {
-    getItemSpy.mockImplementationOnce((key) => {
-      return key === "jwt" ? true : null;
-    });
-
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>,
-    );
-
-    expect(screen.queryByTestId("postBtn")).toBeInTheDocument();
-  });
-
-  it("Renders profile actions button when user is logged in", () => {
-    getItemSpy.mockImplementationOnce((key) => {
-      return key === "jwt" ? true : null;
-    });
-
-    render(
-      <MemoryRouter>
-        <Header />
-      </MemoryRouter>,
-    );
-
-    expect(screen.queryByTestId("profileActBtn")).toBeInTheDocument();
+    expect(screen.getByRole("heading")).toHaveTextContent("QuoteFork");
   });
 });
