e495dcef0dfde1be47c2f6c2995acaffc1852cbc
Samin Ahmed <saminahmed1001@gmail.com>
2026-09-20 22:06:57 +0600

    refactor: Remove profile and post button from the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,59 +1,22 @@
 import { Link } from "react-router";
-import { useState } from "react";
 
 import styles from "./Header.module.css";
 
 // Icons
-import addIcon from "../../assets/icons/add.svg";
-import loginIcon from "../../assets/icons/login.svg";
 
 // Components
-import ProfileActionPopover from "./profileActionPopover/ProfileActionPopover";
+import Navbar from "../navbar/Navbar";
 
-export default function Header() {
-  const [authStatus, setAuthStatus] = useState(localStorage.getItem("jwt"));
-
-  const getNavElements = () => {
-    if (authStatus) {
-      return (
-        <>
-          <button
-            className={`${styles.navPrimaryBtn} postBtn`}
-            title="Create a post"
-            data-testid="postBtn"
-          >
-            <img src={addIcon} alt="Add icon" />
-            <span>Create a post</span>
-          </button>
-
-          <ProfileActionPopover />
-        </>
-      );
-    } else {
-      return (
-        <button
-          className={`${styles.navPrimaryBtn} loginBtn`}
-          title="Login or Sign up"
-          data-testid="loginBtn"
-          onClick={() => {
-            setAuthStatus((prev) => !prev);
-          }}
-        >
-          <img src={loginIcon} alt="Login icon" />
-
-          <span>Login/Sign up</span>
-        </button>
-      );
-    }
-  };
-
+export default function Header({ userData = null }) {
   return (
     <header className={styles.header}>
       <Link className={styles.homeRedirectLink} to={"/"}>
-        <h1 title="Return to the homepage">BlogOdin</h1>
+        <h1 className={styles.heading} title="Return to the homepage">
+          <span style={{ color: "orangered" }}>Q</span>uoteFork
+        </h1>
       </Link>
 
-      <nav className={styles.navbar}>{getNavElements()}</nav>
+      <Navbar userData={userData} />
     </header>
   );
 }
