4f5a702f255995633c44bde93936657f095a5312
Samin Ahmed <saminahmed1001@gmail.com>
2026-09-20 22:11:24 +0600

    feat: Add return to homepage link

--- src/ErrorPage.jsx
+++ src/ErrorPage.jsx
@@ -1,3 +1,10 @@
-export default function ErrorPage(){
-    return <div>Error page</div>
+import { Link } from "react-router";
+
+export default function ErrorPage() {
+  return (
+    <div>
+      Error page
+      <Link to={"/"}>Home</Link>
+    </div>
+  );
 }
