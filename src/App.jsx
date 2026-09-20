1ec681aedd9bf114fdefdb28b212150a26849a63
Samin Ahmed <saminahmed1001@gmail.com>
2026-09-20 22:08:51 +0600

    refactor: Remove theme button

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,37 @@
 // Hooks
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router";
 
 // Components
 import Header from "./components/header/Header";
-import { useState } from "react";
 
 export default function App() {
-  let [theme, setTheme] = useState("light");
+  const storedJWT = localStorage.getItem("jwt");
+
+  let [userData, setUserData] = useState({
+    personalData: null,
+    auth: storedJWT || null,
+  });
+
+  useEffect(() => {
+    if (storedJWT) {
+      // const payloadEncoded = userData.auth.split(".")[1];
+      // const payloadDecoded = JSON.parse(atob(payloadEncoded));
+      // // const userID = payloadDecoded.sub;
+
+      (() => {
+        setUserData((prev) => ({
+          jwt: prev.jwt,
+          personalData: { firstname: "samin", secondName: "ahmed" },
+        }));
+      })();
+    }
+  }, [storedJWT]);
 
   return (
     <>
-      <Header />
-      <Outlet />
-      <button
-        className="themeBtn"
-        onClick={() => {
-          if (theme === "light") {
-            document.body.style.background = "rgb(40, 40, 40)";
-
-            const postCards = document.querySelectorAll(".postCard");
-
-            postCards.forEach((postCard) => {
-              postCard.style.color = "white";
-              postCard.style.background = "rgb(29 29 29)";
-            });
-
-            setTheme("dark");
-
-            localStorage.setItem("theme", "dark");
-          } else {
-            document.body.style.background = "white";
-
-            const postCards = document.querySelectorAll(".postCard");
-
-            postCards.forEach((postCard) => {
-              postCard.style.color = "black";
-              postCard.style.background = "white";
-            });
-
-            setTheme("light");
-
-            localStorage.setItem("theme", "light");
-          }
-        }}
-      >
-        Click
-      </button>
+      <Header userData={userData} />
+      <Outlet context={[userData, setUserData]} />
     </>
   );
 }
