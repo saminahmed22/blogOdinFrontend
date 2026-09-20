ec5115bc3e4d0c57b5237fd8257a412699533924
Samin Ahmed <saminahmed1001@gmail.com>
2026-09-20 22:08:08 +0600

    chore: eslint changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import './index.css'
-import { routes } from './routes'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import "./index.css";
+import { routes } from "./routes";
 
-const router = createBrowserRouter(routes)
+const router = createBrowserRouter(routes);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
