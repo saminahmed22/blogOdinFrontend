02a6e209e8489856da980dc41870fd9f3164c8f3
Samin Ahmed <saminahmed1001@gmail.com>
2026-09-24 22:34:10 +0600

    chore: Add node global options for testing

--- eslint.config.js
+++ eslint.config.js
@@ -14,7 +14,7 @@ export default defineConfig([
       reactRefresh.configs.vite,
     ],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.vitest },
+      globals: { ...globals.browser, ...globals.vitest, ...globals.node },
       parserOptions: { ecmaFeatures: { jsx: true } },
     },
   },
